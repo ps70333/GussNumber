f5e8ad407fedb27db38063e4f837e6ca2a025c68
ps70333 <ps70333@gmail.com>
2017-07-19 21:32:11 +0800

    20170719-03

--- app/src/main/java/tw/com/ei/gussnumber/MainActivity.java
+++ app/src/main/java/tw/com/ei/gussnumber/MainActivity.java
@@ -2,12 +2,25 @@ package tw.com.ei.gussnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnGuss;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGuss=(Button)findViewById(R.id.btnGuss);
+        btnGuss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("SIMON","OK");
+            }
+        });
+    }
+    public void btnOnclick(View view){
+
     }
 }
